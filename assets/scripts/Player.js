c1a3979dd8051751617a84c5ccf25d629c2d610f
lixinxiang <lixinxiang@xiaoyouzi.com>
2018-08-14 17:24:04 +0800

    添加人物铲球交互

---
+++ assets/scripts/Player.js
@@ -0,0 +1,45 @@
+import character from 'aniConfig';
+cc.Class({
+  extends: cc.Component,
+
+  // use this for initialization
+  onLoad: function() {
+    const animation = this.getComponent(cc.Animation);
+    const { finn_red } = character;
+    cc.loader.loadRes('assets/finn_red', cc.SpriteAtlas, (err, atlas) => {
+      const spriteFrames = atlas.getSpriteFrames();
+      for (const key in finn_red) {
+        const element = finn_red[key];
+        let aniFrames = [];
+        spriteFrames.forEach(frame => {
+          const name = frame.name;
+          if (element.indexOf(name) !== -1) {
+            aniFrames.push(frame);
+          }
+        });
+        const clip = cc.AnimationClip.createWithSpriteFrames(aniFrames, 20);
+        clip.name = key;
+        clip.wrapMode = cc.WrapMode.Loop;
+        animation.addClip(clip);
+      }
+      animation.play('stand');
+    });
+  },
+  play() {
+    const animation = this.getComponent(cc.Animation);
+    animation.play('tackle');
+    if (this.node.scaleX > 0) {
+      this.node.x += 100;
+    } else {
+      this.node.x -= 100;
+    }
+
+    setTimeout(() => {
+      animation.play('run');
+    }, 500);
+  }
+  // called every frame, uncomment this function to activate update callback
+  // update: function (dt) {
+
+  // },
+});
